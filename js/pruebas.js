69aff04a2eae184d5d653a5999ed180e07b96627
Luis Alejandro Machado Orozco <luisalejandro.coder@gmail.com>
2023-10-04 15:15:00 -0300

    correcion

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,18 +1,19 @@
-let datoCliente = prompt("Ingrese su nombre y apellido");
-let fechaCita = prompt("ingrese la fecha (formato: DD-MM-AAA)");
-let horaCita = prompt("ingrese la hora formato: HH:MM");
+let nombreUser = prompt("Ingrese su nombre");
+let turnoFecha = prompt("Ingrese Fecha (DD/MM/AAAA)");
+let turnoHora = prompt("Ingrese la Hora (HH:MM)");
 
-const horaAbierto = 9;
-const horaCerrado = 19;
-const horarioAtencion = [];
+const turnos = (nombreUser, turnoFecha, turnoHora) => {
+  const hrInicio = 9;
+  const hrFin = 19;
 
-
-if (horaCita < horaAbierto || horaCita > horaCerrado) {
-    alert(`${datoCliente} Nuestro horario es desde las 9hr hasta las 19hr`);
-} else {
-    alert(`${datoCliente} Gracias por Preferirnos te esperamos el dia ${fechaCita} a las ${horaCita}hr`);
+  if (turnoHora >= hrInicio && turnoHora <= hrFin) {
+    alert(`${nombreUser} tu turno es para ${turnoFecha} a las ${turnoHora}hrs`);
+  } else if (turnoHora < hrInicio || turnoHora > hrFin) {
+    alert("Intenta con una hora dentro de nuestro horario");
+    alert("nuestro horario es de 9hrs a 19hrs");
+  } else {
 }
-horarioAtencion.push(horaAbierto);
+alert("Gracias por preferirnos");
+};
 
-
-
+turnos(nombreUser, turnoFecha, turnoHora);
